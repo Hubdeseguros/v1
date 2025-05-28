bf6879ae9779e9e7cd8b6158a5d8215b579c2dfc
Hubdeseguros <hubdeseguros@hotmail.com>
2025-05-28 19:47:18 +0000

    Deploying to gh-pages from  @ 282477305d8d0b42064e8cc5c798de17d1f74959 🚀

--- _next/static/chunks/app/dashboard/reportes/page-07a73e67082edbc2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[536,931,17,216,706,101,229,481,126,953,606,37,386,363],{2377:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=2377)}),_N_E=n.O()}]);
