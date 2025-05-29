75a76f9d5c5afb2b8224e4f075df341bfda02abd
Hubdeseguros <hubdeseguros@hotmail.com>
2025-05-29 05:14:10 -0400

    Delete __tests__ directory

--- __tests__/simple.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
-test('prueba simple', () => {
-  render(<div>Hola Mundo</div>);
-  expect(screen.getByText('Hola Mundo')).toBeInTheDocument();
-});
